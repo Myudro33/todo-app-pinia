94c548b66d64c890a4c4043d1ea42ddd8744ef96
myudro33 <qanashvilinika7@gmail.com>
2024-08-25 14:18:48 +0400

    feat: create todo component's heading component

---
+++ src/components/Todo/TodoHeading.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full flex flex-row justify-end">
+        <div class="border border-spacing-1 ml-1 p-2 rounded-md">All tasks</div>
+        <div class="border border-spacing-1 ml-1 p-2 rounded-md">Fav tasks</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
