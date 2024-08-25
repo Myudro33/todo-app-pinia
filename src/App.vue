9b3ad90f07c0f17ff2b59c39f6ae0ec3b5d29bf5
myudro33 <qanashvilinika7@gmail.com>
2024-08-25 14:21:42 +0400

    feat: create todos container and wrap over todos and todo heading

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
+import TodosComponent from './components/Todo/TodosContainer.vue';
 </script>
 
 <template>
-  <HeaderComponent/>
+  <HeaderComponent />
+  <TodosComponent />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/Todo/TodosContainer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-1/2 m-auto flex flex-col">
+        <TodoHeading />
+        <h1 class="text-left text-gray-500 my-4">you have many todos left :)</h1>
+        <TodoComponent />
+    </div>
+</template>
+
+<script setup>
+import TodoHeading from './TodoHeading.vue';
+import TodoComponent from './TodoComponent.vue';
+</script>
