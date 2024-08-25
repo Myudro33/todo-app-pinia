5c55fb84a05f41350bb32be6890174242488f92f
myudro33 <qanashvilinika7@gmail.com>
2024-08-25 14:19:46 +0400

    feat: create todo component

---
+++ src/components/Todo/TodoComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full h-20 border rounded-md shadow-md p-5 flex justify-between">
+        <h1 class="text-xl font-bold">some todo</h1>
+        <div class="flex w-20 h-10 justify-end">
+            <img class="cursor-pointer w-8 h-8" src="../../../public/trash-can.png" alt="">
+            <img class="cursor-pointer w-8 h-8 ml-2" src="../../../public/clear-heart.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
