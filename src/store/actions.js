5db243e203afa553e1f3c3d6569afec96202c957
myudro33 <qanashvilinika7@gmail.com>
2024-08-25 21:59:09 +0400

    feat: modify function to delete todo from json server

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,19 +6,33 @@ export default {
     this.todos = data
     this.loading = false
   },
-  deleteTodo(id) {
-    const filteredTodos = this.store.filter((item) => item.id != id)
-    this.todos = filteredTodos
+  async deleteTodo(id) {
+    try {
+      await fetch(`http://localhost:3000/tasks/${id}`, { method: 'DELETE' })
+      const filteredTodos = this.store.filter((item) => item.id != id)
+      this.todos = filteredTodos
+    } catch (error) {
+      alert(error)
+    }
   },
   makeTodoFavorite(id) {
     const favorite = this.todos.findIndex((todo) => todo.id === id)
     this.todos[favorite].isFav = !this.todos[favorite].isFav
   },
   async addTodo(title) {
-    this.todos.push({ id: Math.random(), title: title, isFav: false })
-    await fetch('http://localhost:3000/tasks', {
-      method: 'POST',
-      body: JSON.stringify({ id: Math.random(), title: title, isFav: false })
-    })
+    try {
+      const randomId = String(Math.floor(Math.random() * 999999))
+      this.todos.push({ id: randomId, title: title, isFav: false })
+      await fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        body: JSON.stringify({
+          id: randomId,
+          title: title,
+          isFav: false
+        })
+      })
+    } catch (error) {
+      alert(error)
+    }
   }
 }
